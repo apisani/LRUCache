e912d67bbe6d1428baf8c008190008279bf51bf9
Adrien Pisani <adrien.pisani@asta.com.au>
2018-09-03 22:40:49 +1000

    Cache code commit

---
+++ MemoryCache.Tests/Tests.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using MemoryCache;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCache.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Capacity_Expect_CorrectCapacity()
+        {
+            var cache = new Cache<int, int>(capacity: 100);
+            Assert.That(cache.Capacity, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void IsFull_When_NotFull_Expect_False()
+        {
+            var cache = new Cache<int, int>(capacity:5);
+            cache.AddOrUpdate(0, 10);
+            cache.AddOrUpdate(1, 20);
+            cache.AddOrUpdate(2, 30);
+            cache.AddOrUpdate(3, 40);
+            Assert.False(cache.IsFull);
+        }
+
+        [Test]
+        public void IsFull_When_Full_Expect_True()
+        {
+            var cache = new Cache<int, int>(capacity: 5);
+            cache.AddOrUpdate(0, 10);
+            cache.AddOrUpdate(1, 20);
+            cache.AddOrUpdate(2, 30);
+            cache.AddOrUpdate(3, 40);
+            cache.AddOrUpdate(4, 50);
+            Assert.True(cache.IsFull);
+        }
+
+        [Test]
+        public void Count_Expect_CorrectCount()
+        {
+            var cache = new Cache<int, int>(capacity: 100);
+            cache.AddOrUpdate(0, 10);
+            cache.AddOrUpdate(1, 20);
+            cache.AddOrUpdate(2, 30);
+            cache.AddOrUpdate(3, 40);
+            cache.AddOrUpdate(4, 50);
+            Assert.That(cache.Count, Is.EqualTo(5));
+            cache.AddOrUpdate(5, 60);
+            Assert.That(cache.Count, Is.EqualTo(6));
+
+        }
+
+        [Test]
+        public void Count_When_CacheFull_Expect_CorrectCount()
+        {
+            var cache = new Cache<int, int>(capacity: 5);
+            cache.AddOrUpdate(0, 10);
+            cache.AddOrUpdate(1, 20);
+            cache.AddOrUpdate(2, 30);
+            cache.AddOrUpdate(3, 40);
+            cache.AddOrUpdate(4, 50);
+            Assert.That(cache.Count, Is.EqualTo(5));
+            cache.AddOrUpdate(5, 60);
+            Assert.That(cache.Count, Is.EqualTo(5));
+
+        }
+
+        [Test]
+        public void AddOrUpdate_When_CacheEmpty_Expect_ValueEqualsOutValueInCache()
+        {
+            var cache = new Cache<int, string>(capacity: 100);
+            var key = 0;
+            var value = "First Item";
+            cache.AddOrUpdate(key, value);
+
+            string valueInCache;
+            Assert.True(cache.TryGetValue(key, out valueInCache));
+            Assert.AreEqual(value, valueInCache, "Value in cache does not match value put in");
+        }
+
+        [Test]
+        public void TryGetValue_Expect_FetchCorrectValue()
+        {
+            var cache = new Cache<int, string>(capacity:100);
+            var key = 3;
+            var value = "Item 4";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Item 1");
+            cache.AddOrUpdate(1, "Item 2");
+            cache.AddOrUpdate(2, "Item 3");
+            cache.AddOrUpdate(3, "Item 4");
+            cache.AddOrUpdate(4, "Item 4");
+            cache.AddOrUpdate(5, "Item 4");
+
+            Assert.True(cache.TryGetValue(key, out valueInCache));
+            Assert.AreEqual(value, valueInCache, "Value in cache does not match value put in");
+        }
+
+
+        [Test]
+        //Adding new node with capacity of 5
+        //Expect oldest node (0, "Item 1"), to be removed
+        //and new one (5, "Item 6")to be added to the top of the cache
+        public void TryGetValue_When_CacheFull_Expect_FetchNewAddedValue()
+        {
+            var cache = new Cache<int, string>(capacity:5);
+            var key = 5;
+            var value = "Item 6";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Item 1");
+            cache.AddOrUpdate(1, "Item 2");
+            cache.AddOrUpdate(2, "Item 3");
+            cache.AddOrUpdate(3, "Item 4");
+            cache.AddOrUpdate(4, "Item 5");
+            cache.AddOrUpdate(5, "Item 6");
+
+            Assert.True(cache.TryGetValue(key, out valueInCache));
+            Assert.AreEqual(value, valueInCache, "Value in cache does not match value put in");
+        }
+
+        [Test]
+        //Adding new node when cache is full (capacity = 5)  which now makes (0, "Item 1") the latest node added
+        //Expect oldest node (0, "Item 1"), to be removed
+        //and new one (5, "Item 6")to be added to the top of the cache
+        public void TryGetValue_When__AddingNode_Expect_O1_OldestNodeRemoved()
+        {
+            var cache = new Cache<int, string>(capacity: 5);
+            var key = 0;
+            string value = "Item 1";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Item 1");
+            cache.AddOrUpdate(1, "Item 2");
+            cache.AddOrUpdate(2, "Item 3");
+            cache.AddOrUpdate(3, "Item 4");
+            cache.AddOrUpdate(4, "Item 5");
+            cache.AddOrUpdate(5, "Item 6");
+
+            Assert.False(cache.TryGetValue(key, out valueInCache));
+            Assert.AreNotEqual(value, valueInCache, "Value put in should not exist in cache");
+        }
+
+        [Test]
+        //Retrieving all existing nodes but node (3, "Item 4)", which now become the latest retrieved
+        //Expect oldest node (3, "Item 4"), to be removed
+        //and new one (5, "Item 6")to be added to the top of the cache
+        public void TryGetValue_When_RetrievingNode_Expect_O1_OldestNodeRemoved()
+        {
+            var cache = new Cache<int, string>(capacity: 5);
+            var key = 3;
+            string value = "Item 4";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Item 1");
+            cache.AddOrUpdate(1, "Item 2");
+            cache.AddOrUpdate(2, "Item 3");
+            cache.AddOrUpdate(3, "Item 4");
+            cache.AddOrUpdate(4, "Item 5");
+            
+            cache.TryGetValue(0, out valueInCache);
+            cache.TryGetValue(1, out valueInCache);
+            cache.TryGetValue(2, out valueInCache);
+            cache.TryGetValue(4, out valueInCache);
+
+            cache.AddOrUpdate(5, "Item 6");
+
+            Assert.False(cache.TryGetValue(key, out valueInCache));
+            Assert.AreNotEqual(value, valueInCache, "Value put in should not exist in cache");
+        }
+
+        [Test]
+        //Updating all existing nodes but node (3, "Item 4)", which now become the latest updated
+        //Expect oldest node (3, "Item 4"), to be removed
+        //and new one (6, "Item 7")to be added to the top of the cache
+        public void TryGetValue_When_UpdatingNode_Expect_O1_OldestNodeRemoved()
+        {
+            var cache = new Cache<int, string>(capacity: 5);
+            var key = 3;
+            string value = "Item 4";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Item 1");
+            cache.AddOrUpdate(1, "Item 2");
+            cache.AddOrUpdate(2, "Item 3");
+            cache.AddOrUpdate(3, "Item 4");
+            cache.AddOrUpdate(4, "Item 5");
+
+            cache.AddOrUpdate(0, "Node 1");
+            cache.AddOrUpdate(1, "Node 2");
+            cache.AddOrUpdate(2, "Node 3");
+            cache.AddOrUpdate(4, "Node 5");
+
+            cache.AddOrUpdate(5, "Node 6");
+
+            Assert.False(cache.TryGetValue(key, out valueInCache));
+            Assert.AreNotEqual(value, valueInCache, "Value put in should not exist in cache");
+        }
+
+        [Test]
+        //Updating existing node (same key, different value) a few times
+        //Expect value of the node to be the lastest updated one
+        public void AddOrUpdate_When_KeyIdentical_Expect_ValueToBeUpdated()
+        {
+            var cache = new Cache<int, string>(capacity: 5);
+            var key = 0;
+            string MostUpdatedValue = "Last update for initial node";
+            string valueInCache;
+
+            cache.AddOrUpdate(0, "Initial node");
+            cache.AddOrUpdate(0, "Same node updated");
+            cache.AddOrUpdate(0, MostUpdatedValue);
+
+            Assert.True(cache.TryGetValue(key, out valueInCache));
+            Assert.AreEqual(MostUpdatedValue, valueInCache, "Value in cache does not match value put in");
+        }
+    }
+}
---
+++ MemoryCache/Cache.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace MemoryCache
+{
+    /// <summary>
+    /// Interface to implement for Cache Fone Dymanic Challenge.
+    /// </summary>
+    public interface ICache<TKey, TValue>
+    {
+        /// <summary>
+        /// Adds the value to the cache against the specified key.
+        /// If the key already exists, its value is updated.
+        /// </summary>
+        void AddOrUpdate(TKey key, TValue value);
+        /// <summary>
+        /// Attempts to gets the value from the cache against the specified key
+        /// and returns true if the key existed in the cache.
+        /// </summary>
+        bool TryGetValue(TKey key, out TValue value);
+    }
+
+    /// <summary>
+    /// Memory Cache Fone Dynamics challenge.
+    /// </summary>
+    /// <typeparam name="K">The key type.</typeparam>
+    /// <typeparam name="V">The value type.</typeparam>
+    public class Cache<TKey, TValue> :  ICache<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, CacheNode> _entries;
+        private readonly int _capacity;
+        private CacheNode _head;
+        private CacheNode _tail;
+        private TimeSpan _ttl;
+        private Timer _timer;
+        private int _count;
+
+        /// <summary>
+        /// A least recently used cache using O(1) complexity
+        /// </summary>
+        /// <param name="capacity">
+        /// The maximum number of entries the cache will be able to hold before updating its content
+        /// </param>
+        public Cache(int capacity)
+        {
+            this._capacity = capacity;
+            this._entries = new Dictionary<TKey, CacheNode>(this._capacity);
+            this._ttl = new TimeSpan(0, 0 , 0);
+
+            //First item in cache
+            this._head = null;
+            //Last item in cache
+            this._tail = null;
+
+            this._count = 0;
+            if (this._ttl > TimeSpan.Zero)
+            {
+                this._timer = new Timer(PurgeCache, null, (int)this._ttl.TotalMilliseconds, 6000); // 6 seconds
+            }
+        }
+
+        private class CacheNode
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+            public DateTime LastAccessed { get; set; }
+            public CacheNode Next { get; set; }
+            public CacheNode Previous { get; set; }
+        }
+
+        /// <summary>
+        /// Gets the current number of entries in the cache.
+        /// </summary>
+        public int Count
+        {
+            get { return _entries.Count; }
+        }
+
+        /// <summary>
+        /// Gets the maximum number of entries in the cache.
+        /// </summary>
+        public int Capacity
+        {
+            get { return this._capacity; }
+        }
+
+        /// <summary>
+        /// Gets if the cache if full or not.
+        /// </summary>
+        /// <returns>True if full. False otherwise.</returns>
+        public bool IsFull
+        {
+            get { return this._count == this._capacity; }
+        }
+
+        /// <summary>
+        /// Attempts to gets the value from the cache against the specified key
+        /// </summary>
+        /// <returns>True if the key existed in the cache. False otherwise.</returns>
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            CacheNode entry;
+            value = default(TValue);
+
+            if (!this._entries.TryGetValue(key, out entry))
+            {
+               return false;
+            }
+
+            MoveToHead(entry);
+
+            lock (entry)
+            {
+                value = entry.Value;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Adds the value to the cache against the specified key.
+        /// If the key already exists, its value is updated.
+        /// </summary>
+        /// <param name="key">The cache key.</param>
+        /// <param name="value">The value to set in the cache.</param>
+        public void AddOrUpdate(TKey key, TValue value)
+        {
+            CacheNode entry;
+            if (!this._entries.TryGetValue(key, out entry))
+            {
+                // Add the entry
+                lock (this)
+                {
+                    if (!this._entries.TryGetValue(key, out entry))
+                    {
+                        if (this.IsFull)
+                        {
+                            // Re-use the CacheNode entry
+                            entry = this._tail;
+                            _entries.Remove(this._tail.Key);
+
+                            // Reset with new values
+                            entry.Key = key;
+                            entry.Value = value;
+                            entry.LastAccessed = DateTime.UtcNow;
+
+                            // Next and Previous don't need to be reset.
+                        }
+                        else
+                        {
+                            this._count++;
+                            entry = new CacheNode()
+                            {
+                                Key = key,
+                                Value = value,
+                                LastAccessed = DateTime.UtcNow
+                            };
+                        }
+                        _entries.Add(key, entry);
+                    }
+                }
+            }
+            else
+            {
+                lock (entry)
+                {
+                    entry.Value = value;
+                }
+            }
+
+            MoveToHead(entry);
+
+            if (null == this._tail)
+            {
+                this._tail = this._head;
+            }
+        }
+
+
+        /// <summary>
+        /// Place the given entry to the head of the list.
+        /// </summary>
+        /// <param name="entry">The CacheNode to move up the list.</param>
+        private void MoveToHead(CacheNode node)
+        {
+            if (node == this._head)
+            {
+                return;
+            }
+
+            // Lock is necessary here as we're modifying the entry
+            // which is not thread safe.
+            lock (this)
+            {
+                RemoveCacheNodeFromList(node);
+                AddToHead(node);
+            }
+        }
+
+        private void PurgeCache(object state)
+        {
+            if (this._ttl <= TimeSpan.Zero || this._count == 0)
+            {
+                return;
+            }
+
+            lock (this)
+            {
+                var current = this._tail;
+                var now = DateTime.UtcNow;
+
+                while (null != current && (now - current.LastAccessed) > this._ttl)
+                {
+                    RemoveCacheNode(current);
+                    current = current.Previous;
+                }
+            }
+        }
+
+        private void AddToHead(CacheNode node)
+        {
+            node.Previous = null;
+            node.Next = this._head;
+
+            if (null != this._head)
+            {
+                this._head.Previous = node;
+            }
+
+            this._head = node;
+        }
+
+        private void RemoveCacheNodeFromList(CacheNode node)
+        {
+            var next = node.Next;
+            var previous = node.Previous;
+
+            if (null != next)
+            {
+                next.Previous = node.Previous;
+            }
+            if (null != previous)
+            {
+                previous.Next = node.Next;
+            }
+
+            if (this._head == node)
+            {
+                this._head = next;
+            }
+
+            if (this._tail == node)
+            {
+                this._tail = previous;
+            }
+        }
+
+        private void RemoveCacheNode(CacheNode node)
+        {
+            RemoveCacheNodeFromList(node);
+            this._entries.Remove(node.Key);
+            this._count--;
+        }
+    }
+}
